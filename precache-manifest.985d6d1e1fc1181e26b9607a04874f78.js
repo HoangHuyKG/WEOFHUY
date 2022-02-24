5b5676ae10325d1fc0281be98c67b82dcc8d0b35
unknown <hoanghuywww10@gmail.com>
2022-02-24 22:23:49 +0700

    first commit

---
+++ precache-manifest.985d6d1e1fc1181e26b9607a04874f78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baabbc7714c57a413c4dd926dc42d1eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78e8fbe50569f15d895e",
+    "url": "/static/css/main.fc5dfaa2.chunk.css"
+  },
+  {
+    "revision": "55907a7cb973491e0047",
+    "url": "/static/js/2.b7df0148.chunk.js"
+  },
+  {
+    "revision": "a90bb751b102ed9d8f9443a23ededd32",
+    "url": "/static/js/2.b7df0148.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3d58b8aa31344748448",
+    "url": "/static/js/3.fc39b07c.chunk.js"
+  },
+  {
+    "revision": "78e8fbe50569f15d895e",
+    "url": "/static/js/main.de510be4.chunk.js"
+  },
+  {
+    "revision": "8d0501c8667f84a30ed3",
+    "url": "/static/js/runtime-main.706f781d.js"
+  }
+]);
